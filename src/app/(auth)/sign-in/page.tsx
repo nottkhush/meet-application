4ccd1fe7410416bd47922cdb5e41c6c6eec18e9f
Khushal Jain <khush6569@gmail.com>
2025-08-09 03:11:55 +0530

    04: authentication ui

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import { Card } from "@/components/ui/card";
+import { SignInView } from "@/modules/auth/ui/views/sign-in-view";
+import React from "react";
+
+const SignInPage = () => {
+  return <SignInView />
+};
+
+export default SignInPage;
