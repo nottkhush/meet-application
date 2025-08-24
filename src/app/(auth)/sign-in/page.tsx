1fc5590cc331df345e5c70243d0a032a907106c3
Khushal Jain <khush6569@gmail.com>
2025-08-24 21:33:46 +0530

    05: authentication socials

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,9 +1,19 @@
 import { Card } from "@/components/ui/card";
 import { SignInView } from "@/modules/auth/ui/views/sign-in-view";
 import React from "react";
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
+import { headers } from "next/headers";
 
-const SignInPage = () => {
-  return <SignInView />
+const SignInPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!!session) {
+    redirect("/");
+  }
+  return <SignInView />;
 };
 
 export default SignInPage;
