4f4c15ffd7978ebae9eebcdedb5a4a9431714800
Khushal Jain <khush6569@gmail.com>
2025-09-03 18:22:40 +0530

    14: agent page

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -11,18 +11,26 @@ import {
   MAX_PAGE_SIZE,
   MIN_PAGE_SIZE,
 } from "@/constants";
+import { TRPCError } from "@trpc/server";
 
 export const agentsRouter = createTRPCRouter({
   getOne: protectedProcedure
     .input(z.object({ id: z.string() }))
-    .query(async ({ input }) => {
+    .query(async ({ input , ctx}) => {
       const [existingAgent] = await db
         .select({
           meetingCount: sql<number>`5`,
           ...getTableColumns(agents),
         })
         .from(agents)
-        .where(eq(agents.id, input.id));
+        .where(and(
+          eq(agents.id, input.id),
+          eq(agents.userId, ctx.auth.user.id)
+        ));
+
+    if(!existingAgent){
+      throw new TRPCError({ code: "NOT_FOUND", message: "Agent not found"})
+    }
       return existingAgent;
     }),
 
@@ -57,7 +65,6 @@ export const agentsRouter = createTRPCRouter({
         .limit(pageSize)
         .offset((page - 1) * pageSize);
 
-
       const [total] = await db
         .select({ count: count() })
         .from(agents)
@@ -73,7 +80,7 @@ export const agentsRouter = createTRPCRouter({
       return {
         items: data,
         total: total.count,
-        totalPages
+        totalPages,
       };
     }),
 
