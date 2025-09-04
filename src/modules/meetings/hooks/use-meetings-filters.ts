045d57e992c7b5d14beee1e512b5f68a0f7511e0
Khushal Jain <khush6569@gmail.com>
2025-09-04 22:44:37 +0530

    19: meetings filters

---
+++ src/modules/meetings/hooks/use-meetings-filters.ts
@@ -0,0 +1,15 @@
+import { DEFAULT_PAGE } from "@/constants";
+import { useQueryStates, parseAsString, parseAsInteger, parseAsStringEnum } from "nuqs";
+import { object } from "zod";
+import { MeetingStatus } from "../types";
+
+export const useMeetingsFilters = () => {
+  return useQueryStates({
+    search: parseAsString.withDefault("").withOptions({ clearOnDefault: true }),
+    page: parseAsInteger
+      .withDefault(DEFAULT_PAGE)
+      .withOptions({ clearOnDefault: true }),
+    status: parseAsStringEnum(Object.values(MeetingStatus)),
+    agentId: parseAsString.withDefault("").withOptions({ clearOnDefault: true }),
+  });
+};
